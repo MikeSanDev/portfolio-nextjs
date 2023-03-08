ada404950de348e5e19e8de0201bb4658c15832c
MikeSanDev <mjs070594@gmail.com>
2023-03-07 23:55:53 -0600

    background changes

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,58 +1,96 @@
-import React from 'react'
-import {AiOutlineMail} from 'react-icons/ai'
-import {FaGithub, FaLinkedinIn} from 'react-icons/fa'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Main = () => {
   return (
-    <div id='home' className='w-full h-screen text-center'>
-        <div className='max-w-{1240px} w-full h-full mx-auto p-2 flex justify-center items-center'>
-            <div>
-                <h1 className='py-4 text-gray-700'>
-                    Hi, I'm <span className='text-[#3a6c62]'>Michael</span>
-                </h1>
-                <h1 className='py-2 text-gray-700'>
-                    A Full-Stack Developer
-                </h1>
-                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I am a skilled full-stack developer with a passion for building visually stunning and user-friendly websites. 
-                While my primary expertise is in front-end development, I am constantly expanding my knowledge of back-end technologies to deliver seamless and efficient user experiences. 
-                </p>
-                <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
-                <div className='cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <a className="footer-link" href="https://www.linkedin.com/in/michael-sanchez-825882182/" target="_blank" rel="noopener noreferrer">
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-4 text-[#015055]'>
-                                <FaLinkedinIn size={22} />
-                            </div>
-                        </a>
-                    </div>
-                <div className='cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <a className="footer-link" href="https://github.com/MikeSanDev" target="_blank" rel="noopener noreferrer">
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-4 text-[#015055]'>
-                                <FaGithub size={22} />
-                            </div>
-                        </a>
-                    </div>
-
-                    <div className='cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <a className="footer-link" href="mailto:mike.san.dev16@gmail.com" target="_blank" rel="noopener noreferrer">
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-4 text-[#015055]'>
-                                <AiOutlineMail size={22} />
-                            </div>
-                        </a>
-                    </div>
-
-                    <div className='cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <a className="footer-link" href="/assets/michael_sanchez_resume.pdf" target="_blank" rel="noopener noreferrer">
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-4 text-[#015055]'>
-                                <BsFillPersonLinesFill size={22} />
-                            </div>
-                        </a> 
-                    </div>
+    <div
+      id="home"
+      className="w-full h-screen text-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/assets/light-dark-flip.jpg')" }}
+    >
+      <div className="max-w-{1240px} w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <h1 className="py-4 text-[#ffffff]">
+            Hi, I'm <span className="text-[#ffe590]">Michael</span>
+          </h1>
+          <h1 className="py-2 text-[#ffffff]">A Full-Stack Developer</h1>
+          <p className="py-4 max-w-[70%] text-[#ffffff] m-auto">
+            I am a skilled full-stack developer with a passion for building
+            visually stunning and user-friendly websites. While my primary
+            expertise is in front-end development, I am constantly expanding my
+            knowledge of back-end technologies to deliver seamless and efficient
+            user experiences.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="cursor-pointer hover:scale-110 ease-in duration-300">
+              <a
+                className="footer-link"
+                href="https://www.linkedin.com/in/michael-sanchez-825882182/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div
+                  className="rounded-full shadow-lg shadow-gray-400 p-4 text-[#cebb59]"
+                  style={{ backgroundColor: "#ffffff" }}
+                >
+                  <FaLinkedinIn size={22} />
                 </div>
+              </a>
             </div>
-        </div>
-    </div>
-  )
-}
+            <div className="cursor-pointer hover:scale-110 ease-in duration-300">
+              <a
+                className="footer-link"
+                href="https://github.com/MikeSanDev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div
+                  className="rounded-full shadow-lg shadow-gray-400 p-4 text-[#cebb59]"
+                  style={{ backgroundColor: "#ffffff" }}
+                >
+                  <FaGithub size={22} />
+                </div>
+              </a>
+            </div>
 
-export default Main
+            <div className="cursor-pointer hover:scale-110 ease-in duration-300">
+              <a
+                className="footer-link"
+                href="mailto:mike.san.dev16@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div
+                  className="rounded-full shadow-lg shadow-gray-400 p-4 text-[#cebb59]"
+                  style={{ backgroundColor: "#ffffff" }}
+                >
+                  <AiOutlineMail size={22} />
+                </div>
+              </a>
+            </div>
+
+            <div className="cursor-pointer hover:scale-110 ease-in duration-300">
+              <a
+                className="footer-link"
+                href="/assets/michael_sanchez_resume.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div
+                  className="rounded-full shadow-lg shadow-gray-400 p-4 text-[#cebb59]"
+                  style={{ backgroundColor: "#ffffff" }}
+                >
+                  <BsFillPersonLinesFill size={22} />
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,185 +1,234 @@
-import React, {useState, useEffect} from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import {AiOutlineClose, AiOutlineMenu, AiOutlineMail} from 'react-icons/ai'
-import {FaGithub, FaLinkedinIn} from 'react-icons/fa'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
-import {useRouter} from 'next/router'
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
   const [shadow, setShadow] = useState(false);
-  const [navBg, setNavBg] = useState('#d4d1cf');
-  const [linkColor, setLinkColor] = useState('#1f2937');
+  const [navBg, setNavBg] = useState("#d4d1cf");
+  const [linkColor, setLinkColor] = useState("#1f2937");
   const router = useRouter();
 
   // changes nav button color on individual project pages
-  useEffect(() => { 
+  useEffect(() => {
     if (
-      router.asPath === '/kinetic' ||
-      router.asPath === '/chamber' ||
-      router.asPath === '/whiteElephant' ||
-      router.asPath === '/spotify' 
+      router.asPath === "/kinetic" ||
+      router.asPath === "/chamber" ||
+      router.asPath === "/whiteElephant" ||
+      router.asPath === "/spotify"
     ) {
-      setNavBg('transparent');
-      setLinkColor('#ecf0f3');
+      setNavBg("transparent");
+      setLinkColor("#ecf0f3");
     } else {
-      setNavBg('#d4d1cf');
-      setLinkColor('#1f2937');
+      setNavBg("transparent");
+      setLinkColor("#ffffff");
     }
-  },[router])
+  }, [router]);
 
   const handleNav = () => {
-    setNav(!nav)
-  }
+    setNav(!nav);
+  };
 
   useEffect(() => {
     const handleShadow = () => {
       if (window.scrollY >= 90) {
-        setShadow(true)
+        setShadow(true);
       } else {
-        setShadow(false)
+        setShadow(false);
       }
-    }
-    window.addEventListener('scroll', handleShadow)
-  },[])
+    };
+    window.addEventListener("scroll", handleShadow);
+  }, []);
 
   return (
-    <div 
-    style={{backgroundColor: `${navBg}`}}
-    className={
-      shadow 
-      ? 'fixed w-full h-20 shadow-xl z-[100]' 
-      : 'fixed w-full h-20 z-[100]'
-      }>
-        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-14'> 
-            <Link href='/'>
-            <Image 
-            src="/../public/assets/navLogo.png" 
-            alt="/" 
-            width='100' 
-            height='40'/>
+    <div
+      style={{ backgroundColor: `${navBg}` }}
+      className={
+        shadow
+          ? "fixed w-full h-20 shadow-xl z-[100]"
+          : "fixed w-full h-20 z-[100]"
+      }
+    >
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-14">
+        <Link href="/">
+          <Image
+            src="/../public/assets/navLogo.png"
+            alt="/"
+            width="100"
+            height="40"
+          />
+        </Link>
+        <div>
+          <ul
+            style={{ color: `${linkColor}` }}
+            className="hidden md:flex nav-items pr-6 "
+          >
+            <Link href="/#home">
+              <li className="ml-10 text-sm uppercase">Home</li>
             </Link>
-            <div>
-            <ul style={{color: `${linkColor}`}} className='hidden md:flex nav-items pr-6 '>
-              <Link href='/#home'>
-                <li className='ml-10 text-sm uppercase'>
+            <Link href="/#about">
+              <li className="ml-10 text-sm uppercase">About</li>
+            </Link>
+            <Link href="/#skills">
+              <li className="ml-10 text-sm uppercase">Skills</li>
+            </Link>
+            <Link href="/#projects">
+              <li className="ml-10 text-sm uppercase">Projects</li>
+            </Link>
+            <Link href="/#contact">
+              <li className="ml-10 text-sm uppercase">Contact</li>
+            </Link>
+          </ul>
+          <div onClick={handleNav} className="md:hidden cursor-pointer">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+
+      {/* popup nav */}
+      {/* Nav open/close logic */}
+      <div
+        className={
+          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
+        }
+      >
+        <div
+          className={
+            nav
+              ? "fixed left-0 top 0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#e7e7e7] p-10 ease-in duration-500"
+              : "fixed left-[-120%] top 0  p-10 ease-in duration-500"
+          }
+        >
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <Image
+                src="/../public/assets/navLogo.png"
+                alt="/"
+                width="87"
+                height="35"
+              />
+              <div
+                onClick={handleNav}
+                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+              >
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-4">
+              <p className="w-[85%] md:w-[90%] py-4 text-[#100b0e]">
+                Let's create something that will change the world.
+              </p>
+            </div>
+          </div>
+          <div className="py-3 flex flex-col">
+            <ul className="uppercase">
+              <Link href="/#home">
+                <li
+                  onClick={() => setNav(false)}
+                  className="py-4 text-sm nav-items2"
+                >
                   Home
                 </li>
               </Link>
-              <Link href='/#about'>
-                <li className='ml-10 text-sm uppercase'>
+              <Link href="/#about">
+                <li
+                  onClick={() => setNav(false)}
+                  className="py-4 text-sm nav-items2"
+                >
                   About
                 </li>
               </Link>
-              <Link href='/#skills'>
-                <li className='ml-10 text-sm uppercase'>
+              <Link href="/#skills">
+                <li
+                  onClick={() => setNav(false)}
+                  className="py-4 text-sm nav-items2"
+                >
                   Skills
                 </li>
               </Link>
-              <Link href='/#projects'>
-                <li className='ml-10 text-sm uppercase'>
+              <Link href="/#projects">
+                <li
+                  onClick={() => setNav(false)}
+                  className="py-4 text-sm nav-items2"
+                >
                   Projects
                 </li>
               </Link>
-              <Link href='/#contact'>
-                <li className='ml-10 text-sm uppercase'>
+              <Link href="/#contact">
+                <li
+                  onClick={() => setNav(false)}
+                  className="py-4 text-sm nav-items2"
+                >
                   Contact
                 </li>
               </Link>
             </ul>
-              <div onClick={handleNav} className='md:hidden cursor-pointer'>
-                <AiOutlineMenu size={25}/>
+            <div className="pt-10">
+              <p className="uppercase tracking-widest text-[#cebb59]">
+                Let's Connect
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <div className="cursor-pointer hover:scale-110 ease-in duration-300">
+                  <a
+                    className="footer-link"
+                    href="https://www.linkedin.com/in/michael-sanchez-825882182/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-4 text-[#cebb59]">
+                      <FaLinkedinIn size={22} />
+                    </div>
+                  </a>
+                </div>
+                <div className="cursor-pointer hover:scale-110 ease-in duration-300">
+                  <a
+                    className="footer-link"
+                    href="https://github.com/MikeSanDev"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-4 text-[#cebb59]">
+                      <FaGithub size={22} />
+                    </div>
+                  </a>
+                </div>
+
+                <div className="cursor-pointer hover:scale-110 ease-in duration-300">
+                  <a
+                    className="footer-link"
+                    href="mailto:mike.san.dev16@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-4 text-[#cebb59]">
+                      <AiOutlineMail size={22} />
+                    </div>
+                  </a>
+                </div>
+
+                <div className="cursor-pointer hover:scale-110 ease-in duration-300">
+                  <a
+                    className="footer-link"
+                    href="/assets/michael_sanchez_resume.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-4 text-[#cebb59]">
+                      <BsFillPersonLinesFill size={22} />
+                    </div>
+                  </a>
+                </div>
               </div>
             </div>
-        </div>
-
-
-        {/* popup nav */}
-        {/* Nav open/close logic */}
-        <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''    }>
-          <div className={
-            nav ?  'fixed left-0 top 0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#e7e7e7] p-10 ease-in duration-500' 
-            : 'fixed left-[-120%] top 0  p-10 ease-in duration-500'}>
-              <div>
-                <div className='flex w-full items-center justify-between'>
-                  <Image src='/../public/assets/navLogo.png' alt='/' width='87' height='35'/>
-                  <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
-                    <AiOutlineClose />
-                  </div>
-                </div>
-                <div className='border-b border-gray-300 my-4'>
-                  <p className='w-[85%] md:w-[90%] py-4 text-[#100b0e]'>Let's create something that will change the world.</p>
-                </div>
-              </div>
-              <div className='py-3 flex flex-col'>
-                <ul className='uppercase'>
-                <Link href='/#home'>
-                  <li onClick={() => setNav(false)} className='py-4 text-sm nav-items2'>
-                    Home
-                  </li>
-                </Link>
-                <Link href='/#about'>
-                  <li onClick={() => setNav(false)}  className='py-4 text-sm nav-items2'>
-                    About
-                  </li>
-                </Link>
-                <Link href='/#skills'>
-                  <li onClick={() => setNav(false)} className='py-4 text-sm nav-items2'>
-                    Skills 
-                  </li>
-                </Link>
-                <Link href='/#projects'>
-                  <li onClick={() => setNav(false)} className='py-4 text-sm nav-items2'>
-                    Projects
-                  </li>
-                </Link>
-                <Link href='/#contact'>
-                  <li onClick={() => setNav(false)} className='py-4 text-sm nav-items2'>
-                    Contact
-                  </li>
-                </Link>
-                </ul>
-                <div className='pt-10'>
-                  <p className='uppercase tracking-widest text-[#015055]'>Let's Connect</p>
-                  <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
-                  <div className='cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <a className="footer-link" href="https://www.linkedin.com/in/michael-sanchez-825882182/" target="_blank" rel="noopener noreferrer">
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-4 text-[#015055]'>
-                                <FaLinkedinIn size={22} />
-                            </div>
-                        </a>
-                    </div>
-                <div className='cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <a className="footer-link" href="https://github.com/MikeSanDev" target="_blank" rel="noopener noreferrer">
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-4 text-[#015055]'>
-                                <FaGithub size={22} />
-                            </div>
-                        </a>
-                    </div>
-
-                    <div className='cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <a className="footer-link" href="mailto:mike.san.dev16@gmail.com" target="_blank" rel="noopener noreferrer">
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-4 text-[#015055]'>
-                                <AiOutlineMail size={22} />
-                            </div>
-                        </a>
-                    </div>
-
-                    <div className='cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <a className="footer-link" href="/assets/michael_sanchez_resume.pdf" target="_blank" rel="noopener noreferrer">
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-4 text-[#015055]'>
-                                <BsFillPersonLinesFill size={22} />
-                            </div>
-                        </a> 
-                    </div>
-                  </div>
-                </div>
-              </div>
           </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
