b11d1414acfecff60d7662f796379dd56bd812ef
MikeSanDev <mjs070594@gmail.com>
2024-02-22 15:09:08 -0600

    bg changes

--- components/About.tsx
+++ components/About.tsx
@@ -22,7 +22,9 @@ const About = () => {
   }, [controls, inView]);
 
   return (
-    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16 ">
+    <div id="about" 
+    className="w-full md:h-screen p-2 flex items-center py-16 bg-cover bg-center"
+    style={{ backgroundImage: "url('/assets/bg2.svg')" }}>
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
           <p className="text-xl tracking-widest text-[#ffe590] uppercase">
