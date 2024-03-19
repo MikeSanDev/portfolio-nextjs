06fa4fcc1e88ff2cccd41b255fac8a322d03a57a
MikeSanDev <mjs070594@gmail.com>
2024-03-19 07:56:52 -0500

    skill edits

--- components/Contacts.js
+++ components/Contacts.js
@@ -51,7 +51,7 @@ const Contact = () => {
         <h2 className="py-4 text-slate-50">Get In Touch</h2>
         <div className="grid lg:grid-cols-5 gap-8">
           {/* left */}
-          <div className="bg-gray-200 col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+          <div className="bg-[#5c4c68] col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
             <div className="lg:p-4 h-full">
               <div>
                 <Image
@@ -62,16 +62,16 @@ const Contact = () => {
                   height="400"
                 />
                 <div>
-                  <h2 className="py-3">Michael Sanchez</h2>
-                  <h3 className="text-[#5e6d96]">Full-Stack Developer</h3>
-                  <p className="py-3">
+                  <h2 className="py-3 text-accentColor">Michael Sanchez</h2>
+                  <h3 className="text-secondaryColor">Full-Stack Developer</h3>
+                  <p className="py-3 text-accentColor">
                     I am available for freelance or a full-time position.
                     Contact me to connect!
                   </p>
                 </div>
               </div>
               <div>
-                <p className="pt-8 text-[#5e6d96] text-center">
+                <p className="pt-8 text-secondaryColor text-center">
                   Connect With Me
                 </p>
                 <div className="flex items-center justify-evenly py-4">
@@ -82,7 +82,7 @@ const Contact = () => {
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      <div className="rounded-full shadow-lg shadow-gray-400 p-4 text-[#5e6d96]">
+                      <div className="rounded-full shadow-lg shadow-gray-900 p-4 bg-secondaryColor text-mainColor">
                         <FaLinkedinIn size={22} title="LinkedIn" />
                         <span className="hidden absolute top-0 left-0 bg-white py-2 px-4 text-gray-900 rounded-lg tooltip2">
                           Linkedin
@@ -97,7 +97,7 @@ const Contact = () => {
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      <div className="rounded-full shadow-lg shadow-gray-400 p-4 text-[#5e6d96]">
+                      <div className="rounded-full shadow-lg shadow-gray-900 bg-secondaryColor p-4 text-mainColor">
                         <FaGithub size={22} title="Github" />
                         <span className="hidden absolute top-0 left-0 bg-white py-2 px-4 text-gray-900 rounded-lg tooltip2">
                           Github
@@ -113,7 +113,7 @@ const Contact = () => {
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      <div className="rounded-full shadow-lg shadow-gray-400 p-4 text-[#5e6d96]">
+                      <div className="rounded-full shadow-lg shadow-gray-900 p-4 bg-secondaryColor text-mainColor">
                         <AiOutlineMail size={22} title="Email" />
                         <span className="hidden absolute top-0 left-0 bg-white py-2 px-4 text-gray-900 rounded-lg tooltip2">
                           Email
@@ -129,7 +129,7 @@ const Contact = () => {
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      <div className="rounded-full shadow-lg shadow-gray-400 p-4 text-[#5e6d96]">
+                      <div className="rounded-full shadow-lg shadow-gray-900 p-4 bg-secondaryColor text-mainColor">
                         <BsFillPersonLinesFill size={22} title="Resume" />
                         <span className="hidden absolute top-0 left-0 bg-white py-2 px-4 text-gray-900 rounded-lg tooltip2">
                           Resume
@@ -142,12 +142,12 @@ const Contact = () => {
             </div>
           </div>
           {/* right */}
-          <div className="bg-gray-200 col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
+          <div className="bg-[#5c4c68] col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
             <div className="p-4">
               <form ref={form} onSubmit={sendEmail}>
                 <div className="grid md:grid-cols-1 gap-4 w-full py-2">
                   <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">Name</label>
+                    <label className="uppercase text-accentColor text-sm py-2">Name</label>
                     <input
                       className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
@@ -157,7 +157,7 @@ const Contact = () => {
                   </div>
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Email</label>
+                  <label className="uppercase text-accentColor text-sm py-2">Email</label>
                   <input
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="email"
@@ -166,7 +166,7 @@ const Contact = () => {
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Subject</label>
+                  <label className="uppercase text-accentColor text-sm py-2">Subject</label>
                   <input
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="text"
@@ -175,7 +175,7 @@ const Contact = () => {
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Message</label>
+                  <label className="uppercase text-accentColor text-sm py-2">Message</label>
                   <textarea
                     className="border-2 rounded-lg p-3 border-gray-300"
                     rows={10}
@@ -188,7 +188,7 @@ const Contact = () => {
                   placeholder="Send Message"
                   value="Send Message"
                   className="w-full p-4 text-gray-100 mt-4 bg-gradient-to-r 
-        from-[#d5bcb7] to-[#5e6d96] shadow-xl shadow-gray-400 rounded-xl uppercase cursor-pointer"
+        from-secondaryColor to-mainColor shadow-xl shadow-gray-900 rounded-xl uppercase cursor-pointer"
                 />
               </form>
             </div>
@@ -196,7 +196,7 @@ const Contact = () => {
         </div>
         <div className="flex justify-center py-12">
           <Link href="/">
-            <div className="rounded-full shadow-lg bg-gray-200 shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300 text-[#5e6d96]">
+            <div className="rounded-full shadow-lg  shadow-gray-400 p-4 bg-[#5c4c68] cursor-pointer hover:scale-110 ease-in duration-300 text-accentColor">
               <HiOutlineChevronDoubleUp size={30} className="m-auto" />
             </div>
           </Link>
