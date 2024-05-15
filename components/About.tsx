5c5a7ad4ca3b71d5e334890f47751530d3949e6a
MikeSanDev <mjs070594@gmail.com>
2024-05-15 09:09:20 -0500

    more dance page edits

--- components/About.tsx
+++ components/About.tsx
@@ -54,11 +54,11 @@ const About = () => {
             Check out some of my latest projects I have been working on.
           </Link>
           <br/>
-          <Link href="/dance">
+          {/* <Link href="/dance">
               <p className="underline cursor-pointer text-mainColor py-4 back-project-link">
                 Dance Databank
               </p>
-          </Link>
+          </Link> */}
         </div>
         <motion.div
           ref={ref}
